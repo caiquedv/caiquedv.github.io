a4d57825d8896d2fccf3e87537ef06f3feeaa807
caique <caique.log@gmail.com>
2022-06-29 15:35:27 -0300

    Retirado animação e slider funcionando

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,53 @@
-const Btn = document.querySelector(".scrollbtn");
-        window.addEventListener("scroll", scrollTop);
+const Dqs = (el)=>document.querySelector(el);
+const Dqsa = (el)=>document.querySelectorAll(el);
 
-        function scrollTop() {
-        window.scrollY > 0
-            ? (Btn.style.display = "block") //if ?
-            : (Btn.style.display = "none"); //else :
-        }
-        function subirTela() {
-            window.scrollTo({
-                top: 0,
-                behavior: 'smooth'
-            })
-        }
+// Initial Data - scroll
+
+const Btn = Dqs('.scrollbtn');
+
+// Events
+
+window.addEventListener('scroll', scrollTop);
+
+// Functions
+
+function scrollTop() {
+window.scrollY > 100 ? (Btn.style.display = 'block') : (Btn.style.display = 'none');
+}
+
+function subirTela() {
+    window.scrollTo({
+        top: 0
+    });
+}
+
+// End
+
+// Initial Data - slider
+
+let totalSlides = Dqsa('.slide').length;
+let currentSlide = 0;
+
+Dqs('.slider').style.width = `calc(100% * ${totalSlides})`;
+
+function goPrev() {
+    --currentSlide;
+    if(currentSlide < 0) {
+        currentSlide = totalSlides -1
+    }
+    updateMargin();
+}
+
+function goNext() {
+    currentSlide++;
+    if(currentSlide > totalSlides - 1) {
+        currentSlide = 0;
+    }
+    updateMargin();
+}
+
+function updateMargin() {
+    let sliderItemWidth = Dqs('.slide').clientWidth;
+    let newMargin = (currentSlide * sliderItemWidth);
+    Dqs('.slider').style.marginLeft = `-${newMargin}px`;
+}
