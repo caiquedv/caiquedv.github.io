d3032f6efcaac955d5c4f573e03372ccc5d78ed9
caique <caique.log@gmail.com>
2022-07-21 22:31:10 -0300

    Feature: implantado validação nativa

--- assets/js/script.js
+++ assets/js/script.js
@@ -79,3 +79,81 @@ function menuToogle(){
 }
 
 // End  
+
+// native validator
+
+let validator = {
+    handleSubmit:(event)=>{
+        event.preventDefault();
+        let send = true;
+
+        let inputs = form.querySelectorAll('input');
+        console.log(inputs)
+
+        validator.clearErrors();
+
+        for(let i=0;i<inputs.length;i++) {
+            let input = inputs[i];
+            let check = validator.checkInput(input);
+            if(check !== true) {
+                send = false;
+                validator.showError(input, check);
+            }
+        }
+
+        if(send) {
+            form.submit();
+        }
+    },
+    checkInput:(input) => {
+        let rules = input.getAttribute('data-rules');
+
+        if(rules !== null) {
+            rules = rules.split('|');
+            for(let k in rules) {
+                let rDetails = rules[k].split('=');
+                switch(rDetails[0]) {
+                    case 'required':
+                        if(input.value == '') {
+                            return 'Vazio!';
+                        }
+                    break;
+                    case 'email':
+                        if(input.value != '') {
+                            let regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+                            if(!regex.test(input.value.toLowerCase())) {
+                                return 'Inválido!';
+                            }
+                        }
+                    break;
+                }
+            }
+        }
+
+        return true;
+    },
+    showError:(input, error) => {
+        input.style.borderColor = '#FF0000';
+
+        let errorElement = document.createElement('div');
+        errorElement.classList.add('error');
+        errorElement.style.fontSize = '12px'
+        errorElement.innerHTML = error;
+
+        input.parentElement.insertBefore(errorElement, input.ElementSibling);
+    },
+    clearErrors:() => {
+        let inputs = form.querySelectorAll('input');
+        for(let i=0;i<inputs.length;i++) {
+            inputs[i].style = '';
+        }
+
+        let errorElements = document.querySelectorAll('.error');
+        for(let i=0;i<errorElements.length;i++) {
+            errorElements[i].remove();
+        }
+    }
+};
+
+let form = document.querySelector('form');
+form.addEventListener('submit', validator.handleSubmit);
